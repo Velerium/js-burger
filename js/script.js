33cf4289ecc62ee970acdf581125616480779879
Velerium <bondaniandrea@libero.it>
2021-06-07 14:56:11 +0200

    Added coupon. Whoops~

--- js/script.js
+++ js/script.js
@@ -1,19 +1,30 @@
 var short = document.getElementById('output');
 var food = document.getElementsByClassName('ingredient');
 var price = document.getElementById('price');
+var coupon = document.getElementById('coupon');
 
-var result = 0;
+var toppingPrice = 0;
+var finalPrice = 0;
 
-price.addEventListener('click', totalPrice)
+price.addEventListener('click', priceCalc)
 
-function totalPrice() {
-    result = 0;
+function priceCalc() {
+    toppingPrice = 0;
 
     for (x = 0; x < food.length; x++) {
         if (food[x].checked === true) {
-            result += parseInt(food[x].value);
+            toppingPrice += parseInt(food[x].value);
         }
     }
 
-short.innerHTML = 50 + result + '€';
-}             // 50 is the base price~  Fair deal, huh?
+    finalPrice = 50 + toppingPrice;
+    // 50 is the base price~  Fair deal, huh?
+
+    if (coupon.value === 'BK10') {
+        finalPrice = finalPrice * 0.9;
+    } else if (coupon.value === 'Ottavio') {
+        finalPrice = 0.50 // Ottavio, this is for you~
+    }
+
+short.innerHTML = finalPrice.toFixed(2) + '€';
+}
