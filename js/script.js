cec3d812975205633f9a75d7e110152157e4c536
Velerium <bondaniandrea@libero.it>
2021-06-08 10:56:14 +0200

    Added coupon and burger name interactivity. Fixed HUGE img scaling.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
-var short = document.getElementById('result');
+var burgerName = document.getElementById('sandwich');
+var burgerNameOutput = document.getElementById('burgerName');
 var food = document.getElementsByClassName('ingredient');
 var price = document.getElementById('price');
 var coupon = document.getElementById('coupon');
+var basePrice = 50;
+var sale = 0.9;  // 10% sale
 
 var toppingPrice = 0;
 var finalPrice = 0;
@@ -16,15 +19,24 @@ function priceCalc() {
             toppingPrice += parseInt(food[x].value);
         }
     }
-
-    finalPrice = 50 + toppingPrice;
-    // 50 is the base price~  Fair deal, huh?
-
+    
+    finalPrice = basePrice + toppingPrice;
+    // The base price is 50€~  Fair deal, huh?
+    
     if (coupon.value === 'BK10') {
-        finalPrice = finalPrice * 0.9;
+        finalPrice = finalPrice * sale;
     } else if (coupon.value === 'Ottavio') {
         finalPrice = 0.50; // Ottavio, this is for you~
+    } else if (coupon.value.length > 0) {
+        alert('Invalid code!')
+    }
+    
+    if (burgerName.value.length === 0) {
+        burgerNameOutput.innerHTML = 'Your Burger';
+    } else {
+        burgerNameOutput.innerHTML = burgerName.value;
     }
 
-short.innerHTML = finalPrice.toFixed(2) + '€';
+    document.getElementById('result').innerHTML = finalPrice.toFixed(2) + '€';
 }
+priceCalc();
